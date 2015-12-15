db56b9047d93c016ab464b68510c738668862c27
weiyao86 <weiyaoisnull@gmail.com>
2015-12-15 13:43:42 +0800

    spdm

---
+++ SPDM-EXT.js/js/app/app.js
@@ -0,0 +1,24 @@
+﻿// app.js
+Ext.onReady(function() {
+    var extjsConfig = SPDM.extjsConfig,
+        namespace = SPDM.globalConfig.namespace,
+        startPage = extjsConfig.pages[extjsConfig.pageCode];
+
+    Ext.QuickTips.init();
+
+    Ext.Loader.setConfig({
+        enabled: true,
+        paths: {
+            'Ext.ux': extjsConfig.uxFolder
+        }
+    });
+
+    Ext.application({
+        name: SPDM.globalConfig.namespace,
+        appFolder: extjsConfig.appFolder,
+        controllers: [startPage.controller],
+        launch: function() {
+        	Ext.create(namespace + ".view." + startPage.viewport);
+        }
+    });
+});
